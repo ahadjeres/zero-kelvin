231a861bf6f2c84099c27ca07ec3f03e8739d838
ahadjeres <ahadjeres@gmail.com>
2024-10-01 08:31:59 -0400

    Update create_user.sh
    setting propper permissions on the .ssh folder

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -15,27 +15,26 @@ fi
 USERNAME=$1
 SSH_KEY=$2
 
-# Create a new user without a password and add to sudo group
-adduser --ingroup sudo --disabled-password --gecos "" "$USERNAME"
+# Create a new user without a password and add to the sudo group
+adduser --disabled-password --gecos "" "$USERNAME"
 
-echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USERNAME
-
-# Create .ssh directory for the new user
-mkdir -p /home/"$USERNAME"/.ssh
-
-# Add the SSH public key to the authorized_keys file
-echo "$SSH_KEY" > /home/"$USERNAME"/.ssh/authorized_keys
-
-# Set the correct permissions
-chmod 700 /home/"$USERNAME"/.ssh
-chmod 600 /home/"$USERNAME"/.ssh/authorized_keys
-
-# Change ownership of the .ssh directory and its contents
-chown -R "$USERNAME":"$USERNAME" /home/"$USERNAME"/.ssh
+# Add the new user to the sudo group
+usermod -aG sudo "$USERNAME"
 
 # Configure sudo to not ask for a password for the new user
 echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/"$USERNAME"
 chmod 0440 /etc/sudoers.d/"$USERNAME"
+
+# Create .ssh directory for the new user with correct permissions
+mkdir -p /home/"$USERNAME"/.ssh
+chmod 700 /home/"$USERNAME"/.ssh
+
+# Add the SSH public key to the authorized_keys file
+echo "$SSH_KEY" > /home/"$USERNAME"/.ssh/authorized_keys
+chmod 600 /home/"$USERNAME"/.ssh/authorized_keys
+
+# Change ownership of the home directory and .ssh directory
+chown -R "$USERNAME":"$USERNAME" /home/"$USERNAME"/.ssh
 
 echo "User $USERNAME created, added to sudo group, and SSH key added successfully."
 echo "Sudo configuration updated for user '$USERNAME' to not require a password."
